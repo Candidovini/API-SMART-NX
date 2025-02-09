0542fc51ca42b9d66233a7077dc0e85b7ab69dc0
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-02-09 14:35:21 -0300

    feat: adiciona helper para hash e comparação de senhas

---
+++ src/helpers/Password.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return hashedPassword;
+  } catch (err) {
+    return err;
+  }
+};
+
+const CompareHashPassword = async (password, userPassword) => {
+  try {
+    const comparePassword = await bcrypt.compare(password, userPassword);
+    if (!comparePassword) {
+      throw new Error("Senha Inválida.");
+    }
+    return comparePassword;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = { hashPassword, CompareHashPassword };
