2ee19d3e8a2d757b0154faad3ce1d55376b319ad
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-02-09 14:35:42 -0300

    fix: corrige formatação e adiciona ponto e vírgula no controlador de usuários

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,14 @@
-const { createUser } = require('../services/UsersService');
+const { createUser } = require("../services/UsersService");
 
 const createUserController = async (req, res) => {
   try {
-    const data  = req.body;
+    const data = req.body;
     const response = await createUser(data);
 
     return res.status(201).send(response);
   } catch (error) {
-    return res.status(500).send(error.message)
+    return res.status(500).send(error.message);
   }
 };
-
 
 module.exports = { createUserController };
