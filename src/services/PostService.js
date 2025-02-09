5506ead60f6661e78a8cf0fa43b1ad8a39d9a3b0
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-02-09 16:48:30 -0300

    feat: adiciona funcionalidade para buscar todos os posts com paginação e links

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,7 +1,6 @@
 const { Posts, Users, Comments } = require("../../models");
 
 const create = async (data) => {
-  console.log(data);
   const createPost = await Posts.create(data);
   return createPost;
 };
@@ -21,4 +20,50 @@ const findOne = async (postId) => {
   return findPost;
 };
 
-module.exports = { create, findOne };
+const getAll = async (data) => {
+  const { limit, page } = data.query;
+  const host = data.header("host");
+  const protocol = data.protocol;
+  const pageSize = Number(limit) || 10;
+  const currentPage = Number(page) || 1;
+  const offset = (currentPage - 1) * pageSize;
+
+  const dadosPaginados = await Posts.findAll({
+    limit: pageSize,
+    offset: offset,
+    order: [["createdAt", "ASC"]],
+  });
+
+  const postsWithLinks = dadosPaginados.map((post) => {
+    const baseUrl = new URL(`${protocol}://${host}/post/${post.post_id}`);
+    return {
+      ...post.toJSON(),
+      link: baseUrl,
+    };
+  });
+
+  const totalPosts = await Posts.count();
+  const totalPages = Math.ceil(totalPosts / pageSize);
+
+  const nextPage = currentPage < totalPages ? currentPage + 1 : "";
+  let nextPageLink = "";
+  if (nextPage) {
+    const baseUrl = new URL(`${protocol}://${host}/posts`);
+    baseUrl.searchParams.set("limit", pageSize);
+    baseUrl.searchParams.set("page", nextPage);
+    nextPageLink = baseUrl.toString();
+  }
+
+  return {
+    postsWithLinks,
+    count: postsWithLinks.length,
+    limit,
+    total: totalPosts,
+    currentPage,
+    totalPages: totalPages,
+    nextPage: nextPage,
+    nextPageLink,
+  };
+};
+
+module.exports = { create, findOne, getAll };
