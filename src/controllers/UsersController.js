ded3fb09558a123d074a316bcb8cad7df4e9da73
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-02-10 12:09:03 -0300

    feat: melhora o tratamento de erros e padroniza as respostas no UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,14 @@
 const { createUser } = require("../services/UsersService");
+const { FormatedResponse } = require("../helpers/FormattedResponse");
 
 const createUserController = async (req, res) => {
   try {
     const data = req.body;
-    const response = await createUser(data);
+    await createUser(data);
 
-    return res.status(201).send(response);
+    return res.status(201).send(FormatedResponse.true('User created successfully'));
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(400).send(FormatedResponse.false(error.message));
   }
 };
 
